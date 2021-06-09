307f490608e93ae066937bdc27fcb155424f5bd1
excelon-team <85291106+excelon-team@users.noreply.github.com>
2021-06-09 14:32:00 +0300

    Create XLON.js
    Excelon (XLON) Chain is a POA Ethereum based network providing extreme security, speed, low fees and smart contracts (EVM) features.

---
+++ src/networks/types/XLON.js
@@ -0,0 +1,14 @@
+import clo from '@/assets/images/networks/xlon.svg';
+
+export default {
+  name: 'XLON',
+  name_long: 'Excelon',
+  homePage: 'https://xlon.org/',
+  blockExplorerTX: 'https://explorer.excelon.io/tx/[[txHash]]',
+  blockExplorerAddr: 'https://explorer.excelon.io/account/[[address]]',
+  chainID: 22052002,
+  tokens: tokens,
+  contracts: contracts,
+  icon: xlon,
+  currencyName: 'XLON'
+};
